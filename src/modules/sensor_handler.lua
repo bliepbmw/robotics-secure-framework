655cf085076bb94a4183446a482b8e69a87e01bc
bliepbmw <lukadejanovicstage@gmail.com>
2026-02-26 15:41:26 +0100

    Add obstacle detection logic

--- src/modules/sensor_handler.lua
+++ src/modules/sensor_handler.lua
@@ -1,12 +1,20 @@
--- Sensor Handler
+local Config = require("config")
+local Logger = require("src.utils.logger")
 
 local SensorHandler = {}
 
 function SensorHandler:read_distance()
-    -- Simulated sensor value
     local distance = math.random(10, 100)
-    print("[Sensor] Distance:", distance)
+    Logger:log("INFO", "Distance detected: " .. distance)
     return distance
+end
+
+function SensorHandler:is_obstacle(distance)
+    if distance < Config.SAFE_DISTANCE then
+        Logger:log("WARN", "Obstacle detected")
+        return true
+    end
+    return false
 end
 
 return SensorHandler
