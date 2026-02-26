afd0d883db86e3df6b88cb9197e713f5a12b97d6
bliepbmw <lukadejanovicstage@gmail.com>
2026-02-26 15:40:37 +0100

    Implement PID motor control

--- src/modules/motor_controller.lua
+++ src/modules/motor_controller.lua
@@ -1,14 +1,31 @@
--- Motor Controller
+local Config = require("config")
+local Logger = require("src.utils.logger")
 
 local MotorController = {}
 
-function MotorController:set_speed(left, right)
-    print("[Motor] Left speed:", left)
-    print("[Motor] Right speed:", right)
+local integral = 0
+local previous_error = 0
+
+function MotorController:pid_control(target, current)
+    local error = target - current
+    integral = integral + error
+    local derivative = error - previous_error
+
+    local output = (Config.PID_KP * error) +
+                   (Config.PID_KI * integral) +
+                   (Config.PID_KD * derivative)
+
+    previous_error = error
+
+    return math.min(Config.MOTOR_MAX_SPEED, math.max(0, output))
+end
+
+function MotorController:set_speed(speed)
+    Logger:log("INFO", "Motor speed set to " .. math.floor(speed))
 end
 
 function MotorController:stop()
-    print("[Motor] Emergency stop activated.")
+    Logger:log("WARN", "Emergency stop triggered")
 end
 
 return MotorController
